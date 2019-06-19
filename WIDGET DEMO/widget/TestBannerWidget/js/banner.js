83e38d3da9fcb2b922ed0f2557660cda972efdb5
Aliaksandr Yakubouski <A.Yakubouski@itechart-group.com>
2019-06-19 13:47:21 +0300

    widget

---
+++ WIDGET DEMO/widget/TestBannerWidget/js/banner.js
@@ -0,0 +1,26 @@
+/**
+ * @fileoverview Checkout Cart Summary Widget.
+ *
+ */
+/*global $ */
+/*global define */
+define(
+  
+//-------------------------------------------------------------------
+// DEPENDENCIES
+//-------------------------------------------------------------------
+
+//-------------------------------------------------------------------
+// MODULE DEFINITION
+//-------------------------------------------------------------------
+function() {
+  "use strict";
+
+  return {
+    WIDGET_ID: "TestBannerWidget",
+
+    onLoad: function(widget) {
+      var self = widget;
+    }
+  };
+});
